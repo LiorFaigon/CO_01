acaed4db518e9648e29ceb53f52b1c6341a1f661
Lior Faigon <faigon.lior@gmail.com>
2022-03-04 16:42:26 +0200

    Load raw data from Github repository to dfs Check all genes and samples from raw counts in annotation files ! 32489 genes in raw counts have no annotation data supplement gene annotation df with missing genes with no annotation

--- LF_01.R
+++ LF_01.R
@@ -1,2 +1,42 @@
 # Champions Oncology - Exercise for bioinformatics candidates
 # Submitted by Lior Faigon
+
+#Load required libraries:
+#library(Biobase) # for ExpressionSet
+
+
+# Load the data into R and make sure the count and annotation data are consistent with each other.
+
+raw_counts = read.csv("https://raw.githubusercontent.com/LiorFaigon/CO_01/master/counts.txt",
+                      header = T,
+                      row.names = 1,
+                      sep = "\t")
+gene_annotations = read.csv("https://raw.githubusercontent.com/LiorFaigon/CO_01/master/gene-annotation.txt",
+                            header = T,
+                            sep = "\t")
+colnames(gene_annotations)[4] = "NAME" # fill empty colname in gene_annotations
+sample_annotations = read.csv("https://raw.githubusercontent.com/LiorFaigon/CO_01/master/sample-annotation.txt",
+                            header = T,
+                            row.names = 1,
+                            sep = "\t")
+
+raw_counts_samples = as.list(colnames(raw_counts))
+raw_counts_genes = as.list(rownames(raw_counts))
+gene_annotations_entries = as.list(gene_annotations$ENSEMBL)
+sample_annotations_entries = as.list(rownames(sample_annotations))
+
+length(setdiff(raw_counts_samples, sample_annotations_entries)) #0
+length(setdiff(sample_annotations_entries, raw_counts_samples)) #0
+length(setdiff(raw_counts_genes, gene_annotations_entries)) #32489
+length(setdiff(gene_annotations_entries, raw_counts_genes)) #0
+
+genes_missing_annotation = c(raw_counts_genes[!(raw_counts_genes %in% gene_annotations_entries)])
+gene_annotations_supplemented = data.frame("SYMBOL" = vector(mode="character", length=length(genes_missing_annotation)),
+                                           "NAME" = vector(mode="character", length=length(genes_missing_annotation))
+                                           )
+gene_annotations_supplemented$ENSEMBL = genes_missing_annotation
+gene_annotations_supplemented$ENTREZID = NA
+gene_annotations_supplemented = gene_annotations_supplemented[,c(3,4,1,2)]
+gene_annotations_supplemented = rbind(gene_annotations, gene_annotations_supplemented)
+
+
